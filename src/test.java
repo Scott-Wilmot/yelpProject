12cc73346da77c5366d2d801e1782e550f247ac0
Gooseclemons <scottwilmot04@gmail.com>
2024-04-09 02:13:57 -0400

    Added persistent hash table class

--- src/test.java
+++ src/test.java
@@ -4,108 +4,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
 public class test {
 
-    static String serializedObjectName = "cereal.ser";
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-    static void serializeObject(Object o, String filePath) {     //For when a serialized obj file has not been created yet or can't be found
-        try {
-            FileOutputStream fileOut = new FileOutputStream(filePath);
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-            out.writeObject(o);
-            out.close(); fileOut.close();
-            System.out.printf("Serialized Object saved to: %s%n", serializedObjectName);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+        IndexArray ia = new IndexArray();
+        ia.put("spurg");
 
-    static Object deserializeObject(String fileName) {
-        Object o = null;
-        try {
-            FileInputStream fileIn = new FileInputStream(fileName);
-            ObjectInputStream in = new ObjectInputStream(fileIn);
-            o = in.readObject();
-            in.close();
-            fileIn.close();
-        } catch (IOException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        return o;
-    }
-
-    public static void main(String[] args) {
-
-//        ArrayList<Person> people = new ArrayList<>();
-//        people.add(new Person("Scott", 19));
-//        people.add(new Person("Maddy", 19));
-//        people.add(new Person("Dijiste", 100));
-//
-//        String outputFilePath = "C:\\Users\\GooseAdmin\\IdeaProjects\\yelpProject\\testOutput";
-//        File file = new File(String.format(outputFilePath + File.separator + serializedObjectName));
-//        System.out.println(file);
-//
-//        if (file.isFile()) {    //Serializable exists already
-//            ArrayList<Person> personArrayList = (ArrayList<Person>) deserializeObject(file.toString());
-//            for (Person p : personArrayList) {
-//                System.out.println(p.name + ", " + p.age);
-//            }
-//        } else {
-//            serializeObject(people, file.toString());
-//        }
-
-        // Create int list and groups as well as remove the medoid values from the int list
-//        ArrayList<Integer> ints = new ArrayList<>(Arrays.asList(0,1,2,3,4,5,6,7,8,9));
-//        ArrayList<Group> groupList = new ArrayList<>();
-//        Group g1 = new Group(1); groupList.add(g1);
-//        Group g2 = new Group(7); groupList.add(g2);
-//        Group g3 = new Group(9); groupList.add(g3);
-//        ints.remove(Integer.valueOf(1)); ints.remove(Integer.valueOf(7)); ints.remove(Integer.valueOf(9));
-//
-//
-//        // This algorithm favors the closestGroup that is already established when it comes to a tie
-//        for (int val : ints) { // Go through values to cluster
-//            Group closestGroup = null; // Keeps track of the group that the int should be assigned too
-//            for (Group g : groupList) {
-//                if (closestGroup == null) {closestGroup = g;} //Base case
-//                else if (Math.abs(g.center - val) < Math.abs(closestGroup.center - val)) { // If current group is closer than closestGroup...
-//                    closestGroup = g;
-//                }
-//            }
-//            closestGroup.addToGroup(val); // With the closest group found, add val to groups values
-//        }
-//
-//        System.out.println("Group 1:");
-//        for (int a : g1.group) {
-//            System.out.println(a);
-//        }
-//        System.out.println("Group 2:");
-//        for (int a : g2.group) {
-//            System.out.println(a);
-//        }
-//        System.out.println("Group 3:");
-//        for (int a : g3.group) {
-//            System.out.println(a);
-//        }
-
-    }
-
-}
-
-class Person implements Serializable {
-
-    String name;
-    int age;
-
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String toString() {
-
-        return name + ", " + age;
     }
 
 }
